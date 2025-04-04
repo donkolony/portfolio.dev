843f36a76fdf512a23bac4e60edfef526100e13b
donkolony <donkolony@gmail.com>
2025-04-04 12:40:45 +0200

    Update light theme color

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Footer Links scroll up and not down
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopLinks = document.querySelectorAll(".scroll-to-top");
+
+  scrollToTopLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent default link behavior
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", // For smooth scrolling
+      });
+    });
+  });
+});
