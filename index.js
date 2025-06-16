60ecc1b3d630b53976d5aa7fcb9a384430ccf174
donkolony <donkolony@gmail.com>
2025-06-16 14:16:04 +0200

    update UI

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Hamburger Menu Toggle
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
+  const menu = document.querySelector(".menu-list");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
